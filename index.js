35fad49ab2e141430fecda74e2543cba65121e75
Jain-Daksh <daksh44jain@gmail.com>
2022-05-04 14:33:08 +0530

    changes in index file

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const app = express()
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 const PORT = process.env.PORT || 3000;
 
-//require('dotenv').config()
-//console.log(process.env.db_username)
-//console.log(process.env.PORT)
+
 
 app.get('/' , (req,res) => {
     res.send("hi")
